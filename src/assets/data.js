082b9e07b4a2165e12d69a53da7bb8c5e9511c24
javawillch <javawillch@gmail.com>
2021-04-17 00:36:00 +0800

    add data.js

---
+++ src/assets/data.js
@@ -0,0 +1,39 @@
+window.Project = [
+    {  
+        name: "Cube made by DAX",
+        comment: "Trend Micro",
+        img: "./../assets/Cube.gif",
+        type: "gif"
+    },
+    {
+        name: "Global Data Warehouse Pipeline",
+        comment: "Trend Micro",
+        img: "./../assets/DataFlow.gif",
+        type: "gif"
+    },
+    {
+        name: "Audit System for Banking",
+        comment: "HY Tech",
+        img: "./../assets/AuditSystem.gif",
+        type: "gif"   
+    },
+    {
+        name: "Babe Care Website",
+        comment: "III",
+        img: "./../assets/BabeQ.mp4",
+        type: "mp4"   
+    },
+    {
+        name: "Bungee Jumping",
+        comment: "in Macau",
+        img: "./../assets/BungeeJumping.gif",
+        type: "gif"   
+    },    
+    {
+        name: "Skydiving",
+        comment: "in Cairns",
+        img: "./../assets/Skydiving.gif",
+        type: "gif"   
+    }
+]
+
